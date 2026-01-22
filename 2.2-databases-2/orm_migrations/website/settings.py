59717aad9673651b41a9d90bb81a43b9c4462656
Vladimir1mir <korneevvladimira@gmail.com>
2026-01-22 19:43:18 +0300

    Add files via upload

--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = []
 
 SITE_ID = 1
 
-
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 # Application definition
 
 INSTALLED_APPS = [
@@ -74,6 +74,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
+        'USER': 'postgres',
+        'PASSWORD': '1968',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
@@ -102,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Moscow'
 
